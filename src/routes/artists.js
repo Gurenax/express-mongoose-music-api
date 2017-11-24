fcab3fcca1a9736d8ed9d3e0e1650a5683d4a253
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-24 15:03:22 +1100

    Add solution to challenges 1 to 3

--- src/routes/artists.js
+++ src/routes/artists.js
@@ -12,7 +12,7 @@ router.get('/artists', (req, res) => {
       res.json(artists)
     })
     .catch(error => {
-      res.status(400).json({error: error.message})
+      res.status(400).json({ error: error.message })
     })
 })
 
@@ -29,13 +29,71 @@ router.get('/artists/:id', (req, res) => {
       }
       // If no artist was foound
       else {
-        res.status(404).json({error: `Artist not found with id: ${id}`})
+        res.status(404).json({ error: `Artist not found with id: ${id}` })
       }
     })
     .catch(error => {
       // If there was an error, most likely with the format of the id
-      res.status(400).json({error: error.message})
+      res.status(400).json({ error: error.message })
     })
+})
+
+// Create
+router.post('/artists', (req, res) => {
+  const attributes = req.body
+  if(attributes.name) {
+    Artist.create(attributes)
+      .then(artist => {
+        res.status(201).json(artist)
+      })
+      .catch(error => {
+        res.status(400).json({ error: error })
+      })
+  }
+  else {
+    res.status(403).json({ error: 'Name is required' })
+  }
+})
+
+// Update
+router.patch('/artists/:id', (req, res) => {
+  const id = req.params.id
+  const attributes = req.body
+  Artist.findByIdAndUpdate(id, attributes, {new: true})
+    .then(artist => {
+      // If an artist was found and updated
+      if(artist) {
+        
+        res.status(200).json(artist) //{artist: artist, update: attributes})
+      }
+      // If no artist was found
+      else {
+        res.status(404).json({ error: `Artist not found with id: ${id}` })
+      }
+    })
+    .catch(error => {
+      res.status(400).json({ error: error })
+    })
+})
+
+// Destroy
+router.delete('/artists/:id', (req, res) => {
+  const id = req.params.id
+  Artist.findByIdAndRemove(id)
+    .then(artist => {
+      // If an artist was found and deleted
+      if(artist) {
+        res.status(200).json(artist)
+      }
+      // If no artist was found
+      else {
+        res.status(404).json({ error: `Artist not found with id: ${id}` })
+      }
+    })
+    .catch(error => {
+      res.status(400).json({ error: error })
+    })
+  
 })
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const server = express()
+
+// Allow POST/PUT/PATCH requests to pass along JSON, and it will be decoded for us
+// and it will be decoded for us at 'req.json'
+server.use(bodyParser.json())
 
 server.use('/',[
   require('./routes/artists')
